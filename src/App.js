e6059d62ede20e1639db123653ccddbb96641ab8
keji <756715851@qq.com>
2023-01-12 11:32:56 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./style.css"
+
+export default function App() {
+  return (
+    <>
+      <div className="wra">
+        <div className="box"></div>
+      </div>
+      <div className="wrapper">
+        <div className="hour"></div>
+        <div className="minute"></div>
+        <div className="second"></div>
+      </div>
+    </>
+  )
+}
